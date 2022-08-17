693d73912e31617ab992a6824c2033060e913066
mhkim <mhkim@straffic.co.kr>
2022-08-18 07:49:51 +0900

    상황판 미오인식 데이터 불러오기 추가 미오인식 유형분류 조회시 날짜 추가 라벨링 확정시 미사용 컬럼 삭제

--- src/main/java/com/egov/ex/controller/DashboardController.java
+++ src/main/java/com/egov/ex/controller/DashboardController.java
@@ -1,22 +1,21 @@
 package com.egov.ex.controller;
 
 
-import com.egov.ex.dto.AjaxResp;
-import com.egov.ex.dto.LoginVo;
-import com.egov.ex.dto.UserTokenVo;
-import com.egov.ex.entity.Dashboard;
-import com.egov.ex.entity.UserInfo;
-import com.egov.ex.service.DashboardService;
-import com.egov.ex.util.AppConfig;
-import com.egov.ex.util.CookieUtils;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
+import java.util.List;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.egov.ex.dto.AjaxResp;
+import com.egov.ex.entity.Dashboard;
+import com.egov.ex.service.DashboardService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 /**
  * <p>
@@ -45,4 +44,19 @@ public class DashboardController {
         return AjaxResp.success(dashboard);
 
     }
+    
+    @ApiOperation("대쉬보드")
+    @PostMapping("/api/dashboard/nmrecg")
+    public AjaxResp nmrecg() throws Exception {
+        List<HashMap> dashboard = dashboardService.selectNmrecg();
+        return AjaxResp.success(dashboard);
+    }
+    
+    
+//    @PostMapping(value="/api/crgw-img-data/list")
+//    public AjaxResp crgwImgList(@RequestBody CrgwImgDataSearchData param) throws Exception {
+////        param.setWorkDate("2022-05-30"); //test
+//        PageUtil.startPage(param);
+//        List<CrgwImgData> list = crgwImgDataService.selectCrgwImgDataList(param);
+//        return AjaxResp.success(PageUtil.of(list));
 }
